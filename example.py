7e5c49f0723c606887a5a00455f59e4404a29f38
suzukaotto <me@szk.kr>
2024-11-17 17:44:01 +0900

    update: example update

--- example.py
+++ example.py
@@ -3,11 +3,11 @@ import everline_api
 
 api_instance = everline_api.EverlineAPI()
 
-# api_instance.get_data()
-# datas = api_instance.get_train_info()
+api_instance.get_data()
+datas = api_instance.get_train_info()
 
-for h in range(5, 24):
-    for m in range(60):
-        current_time = f"{h:02d}{m:02d}"
-        print(current_time, everline_api.get_train_interval(current_time))
-        time.sleep(0.1)
+for data in datas:
+    print(data['TrainNo'], end=" ")
+    print("상행" if data["updownCode"] == everline_api.TRAIN_UPWARD else "하행", end=" ")
+    print(f"{data['StCode']}", end=" ")
+    print(data["driveRate"])
